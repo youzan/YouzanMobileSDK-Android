14a36d476f49e35827b21556d5acc3169ccfc07b
heiyu <heiyu@youzan.com>
2021-05-06 19:47:49 +0800

    版本上传到有赞云 basic 7.0.2

--- YouzanBasicSample/src/main/java/com/youzanyun/sdk/sample/basic/YouzanActivity.java
+++ YouzanBasicSample/src/main/java/com/youzanyun/sdk/sample/basic/YouzanActivity.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+
 public class YouzanActivity extends AppCompatActivity {
     public static final String KEY_URL = "url";
     private YouzanFragment mFragment;
